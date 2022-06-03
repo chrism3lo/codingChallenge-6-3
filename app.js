09c23e1277ecca06c803090e1fafd9b0883eedd3
chrism3lo <christopher.melopelaez@gmail.com>
2022-06-03 14:17:00 -0400

    completed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 // let date1 = 'June 14 2019';
 // let date2 = 'June 20 2019';
-// let result;
+
 
 // function getDays(date1,date2){
 //     dateArr = date1.split(" ");
@@ -18,15 +18,37 @@
 
     // answer with "/"
 
-    let date1 = "06/14/2019";
-    let date2 = "06/20/2019";
+    // let date1 = "06/14/2019";
+    // let date2 = "06/20/2019";
 
-    function getDays(date1,date2){
-            dateArr = date1.split("/");
-            dateArr2 = date2.split("/")
+    // function getDays(date1,date2){
+    //         dateArr = date1.split("/");
+    //         dateArr2 = date2.split("/")
 
-            return Math.abs(Number(dateArr[1] - dateArr2[1]));
-        }
+    //         return Math.abs(Number(dateArr[1] - dateArr2[1]));
+    //     }
 
-        //console.log(getDays(date1))
-        console.log(getDays(date1,date2))
+    //     //console.log(getDays(date1))
+    //     console.log(getDays(date1,date2))
+
+
+// answer with comma
+
+let date1 = 'June 14, 2019';
+let date2 = 'June 20, 2019';
+
+
+function getDays(date1,date2){
+    dateArr = date1.split(" ");
+    dateArr2 = date2.split(" ")
+
+    newArr1 = dateArr[1].split(",");
+    newArr2 = dateArr2[1].split(",");
+
+    return Math.abs(Number(newArr1[0] - newArr2[0]));
+    
+}
+
+
+
+    console.log(getDays(date1,date2));
